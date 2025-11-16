9130d6b8a0ef344c39e35e4ce4e12f414deb97f0
Sudhanshu Garg <sudhanshug@gmail.com>
2025-11-16 12:30:56 -0800

    adding token embeddings

--- pytorch/self_attention_module.py
+++ pytorch/self_attention_module.py
@@ -4,31 +4,39 @@ import torch.nn.functional as F
 import math
 
 class SelfAttentionModule(nn.Module):
-    def __init__(self, d_model: int, query_key_dim: int, seq_length: int):
+    def __init__(self, 
+                 d_model: int, 
+                 query_key_dim: int, 
+                 seq_length: int) -> None:
         super (SelfAttentionModule, self).__init__()
         self.d_model = d_model
         self.qk_dim = query_key_dim
         self.seq_length = seq_length
 
+        #self.token_embedding = token_embedding
         self.q = nn.Linear(d_model, query_key_dim)
         self.k = nn.Linear(d_model, query_key_dim)
         self.v = nn.Linear(d_model, d_model)
         
         #TODO mlp: [gate_proj, up_proj, down_proj] matrices
 
-    def forward(self, x):
-        # is x a set of tokens or a matrix
-        # [batch_size, seq_length]
-        # or is it a set of token embeddings
-        # of [batch_size, seq_length, d_model] ?
-        # Lets assume latter for now.: [batch_size, seq_length, d_model]
+    def forward(self, x: torch.Tensor):
+        # x_token is a sequence of seq_length tokens
+        # and we have a batch
+        # x_token is [batch_size, seq_length]
 
-        Q = torch.matmul(x, self.q)
+        # we need to lookup the corresponding embedding of each token
+        
+        #x = self.token_embedding(x_token.to(torch.int64)) #[batch_size, seq_length, d_model]
+        #print(f"x_token: {x_token.shape}, x: {x.shape}")
 
-        # Q = torch.matmul(x, self.q.transpose(-1))
-        # K = torch.matmul(x, self.k.transpose(-1))
-        # attention = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_model)
+        Q = self.q(x) # [batch_size, seq_len, query_key_dim]
+        K = self.k(x) # [batch_size, seq_len, query_key_dim]
+        attention = torch.matmul(Q, K.transpose(1, 2)) / math.sqrt(self.d_model) # [batch_size, seq_len, seq_len]
+        #print(f"Q: {Q.shape}, K: {K.shape}, attention: {attention.shape}")
 
-        # multiply by mask
-        # then softmax
-        # then multiply by embeddings and sum up.
+        #TODO masking
+        soft_maxed = F.softmax(attention, dim=2) # [batch_size, seq_len, seq_len]
+        V = self.v(x) # [batch_size, seq_len, d_model]
+
+        return torch.matmul(soft_maxed, V) # [batch_size, seq_len, d_model]
--- pytorch/trainer.py
+++ pytorch/trainer.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from simple_model import SimpleModel
 from simple_dataset import SimpleDataset
+from self_attention_module import SelfAttentionModule
 from torch.utils.data.dataloader import DataLoader
 
 
@@ -11,23 +12,29 @@ class Trainer():
     def __init__(self):
         pass
 
-    def train(self, model: nn.Module, dataloader: DataLoader, max_epoch: int = 5):
+    def train(self, model: nn.Module, token_embedding: nn.Embedding, dataloader: DataLoader, max_epoch: int = 5):
         device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
         
         model = model.to(device)
-        optimizer = optim.Adam(model.parameters(), lr=1e-4)
+        optimizer = optim.Adam(list(model.parameters()) + list(token_embedding.parameters()), lr=1e-4)
 
         model.train()
         step = 0
         with torch.enable_grad():
             for _ in range(max_epoch):
                 for batch_idx, (X, y) in enumerate(dataloader):
-                    # print(f"batch {batch_idx}: {X.shape}, {y.shape}")
-                    X_device = X.to(device)
+                    #print(f"batch {batch_idx}: {X.shape}, {y.shape}")
+                    #with token dims
+                    X_token = token_embedding(X.to(torch.int64)) #[batch, seq, d_model]
+                    y_token = token_embedding(y.to(torch.int64)) #[batch, seq, d_model]
+                    #print(f"X_token: {X_token.shape}, X: {X.shape}")
+
+                    X_device = X_token.to(device) #[batch, seq]
+
                     pred = model(X_device)
                     #print(f"pred = {pred}")
                     #loss = self.compute_loss(pred, y)
-                    y_device = y.to(device)
+                    y_device = y_token.to(device)
                     loss = F.mse_loss(pred, y_device)
                     if step % 10 == 0:
                         print(f"step: {step}, loss = {loss}")
@@ -42,14 +49,33 @@ class Trainer():
         pass
 
 
-def main():
+def attention():
+    input_size = 3
+    dataset = SimpleDataset(seq_length=input_size, file_path="./data.txt")
+    print(f"num_chars={dataset.__len__()}")
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
+    vocab_size = dataset.__len__() + 1
+    d_model = 1024
+    token_embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+
+    
+    trainer = Trainer()
+    model = SelfAttentionModule(d_model=d_model, query_key_dim=128, seq_length=input_size)
+
+    print(token_embedding.weight[0:5, 0:3])
+    trainer.train(model=model, token_embedding=token_embedding, dataloader=dataloader, max_epoch=50)
+    print(token_embedding.weight[0:5, 0:3])
+
+def feedForward():
     input_size = 2
     dataset = SimpleDataset(seq_length=input_size, file_path="./data.txt")
     dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
     
     trainer = Trainer()
     model = SimpleModel(input_size, input_size, hidden_layers=5, hidden_layer_dim=16)
-    trainer.train(model=model, dataloader=dataloader, max_epoch=15)
+
+    trainer.train(model=model, dataloader=dataloader, max_epoch=20)
 
 if __name__ == "__main__":
-    main()
+    # feedForward()
+    attention()
